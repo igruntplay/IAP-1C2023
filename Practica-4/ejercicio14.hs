7799cfd58adf6d46420fc1cfd3c525a14df03d7d
Grunt <grunt@pop-os.localdomain>
2023-04-20 20:27:44 -0300

    agrego ejercicio 14

---
+++ Practica-4/ejercicio14.hs
@@ -0,0 +1,8 @@
+-- implementar una funcion suma potencias
+sumaPotencias :: Integer -> Integer -> Integer -> Integer
+sumaPotencias q 1 b = sumaPotenciasAux q 1 b
+sumaPotencias q a b = sumaPotencias q (a-1) b + sumaPotenciasAux q a b
+
+sumaPotenciasAux :: Integer -> Integer -> Integer -> Integer
+sumaPotenciasAux q a 1 = q^(a+1)
+sumaPotenciasAux q a b = q^(a+b) + sumaPotenciasAux q a (b-1)
