681ead996546456a5f4ae748f6455564be066a9e
igruntplay <gaspar.onesto@gmail.com>
2023-05-04 12:40:16 -0300

    agrego ejercicios resueltos del ejercicio 4

--- Practica-5/Ejercicio-4/ejercicio2.hs
+++ Practica-5/Ejercicio-4/ejercicio2.hs
@@ -12,3 +12,15 @@ contarPalabras2 [] = 0
 contarPalabras2 [x] = 1
 contarPalabras2 (x:xs) | x == ' ' = 1 + contarPalabras2 xs
                        | otherwise = contarPalabras2 xs
+{-
+aux :: [Char] -> [[Char]]
+aux [] = [[]]
+"uhdfksn faefg ogllg      " -> [[uhdfksn], [faefg], [ogllg]]
+
+borrarEspaciosDelFinal :: "
+
+1ro necesito un auxiiliar que me revierta el orden de la lista al reves
+funcion revertir
+2do necesito un auxiliar que me quite los espacios del prinicpio
+entonces seria contarpalabras quitar espacios del principio revertir quitar espacios del principio
+-}
--- Practica-5/Ejercicio-4/ejercicio4.hs
+++ Practica-5/Ejercicio-4/ejercicio4.hs
@@ -1,4 +1,21 @@
 palabras :: [Char] -> [[Char]]
 palabras [] = []
-palabras [x] = [[x]]
-palabras (x:xs) = 
+palabras (x:xs) = palabrasAux(sacoPrimerEspacio( sacarBlancosRepetidos (x:xs)))
+    where
+        palabrasAux (x:xs) = primerPalabra (x:xs) : palabras (sacarPalabra (x:xs))
+        sacoPrimerEspacio (x:xs) | x == ' ' = xs
+                                 | otherwise = (x:xs)
+primerPalabra :: [Char] -> [Char]
+primerPalabra [] = []
+primerPalabra (x:xs) | x == ' ' = []
+                     | otherwise = x : primerPalabra xs
+
+sacarPalabra :: [Char] -> [Char]
+sacarPalabra [] = []
+sacarPalabra (x:xs) | x == ' ' = xs
+                     | otherwise = sacarPalabra xs
+sacarBlancosRepetidos :: [Char] -> [Char]
+sacarBlancosRepetidos [] = []
+sacarBlancosRepetidos [x] = [x]
+sacarBlancosRepetidos (x:y:xs) | x == ' ' && y == ' ' = sacarBlancosRepetidos (y:xs)
+                               | otherwise = x : sacarBlancosRepetidos (y:xs)
