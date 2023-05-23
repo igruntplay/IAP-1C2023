27db3ab804488b01bd69dc7b92746127a84e1fcd
igruntplay <gaspar.onesto@gmail.com>
2023-05-23 17:49:35 -0300

    sumo hasta ejercicio 7

--- Practica-8/ejercicio1.py
+++ Practica-8/ejercicio1.py
@@ -37,7 +37,7 @@ def sumaTotal(s:array)->bool:
 
 #print(sumaTotal([1,1,1]))
 
-## PREGUNTAR ORDENADOS
+## PREGUNTAR ORDENADOS ejercicio 4
 
 def ordenados(s:array)->bool:
     for i in range(len(s)-1):
@@ -45,4 +45,29 @@ def ordenados(s:array)->bool:
             return False
     return True
 
-print(ordenados([0,0,2]))
+#print(ordenados([0,0,2]))
+
+## Ejercicio 5
+def listaDePalabras(palabras:list[str])->bool:
+    for palabra in palabras:
+        if len(palabra)>7:
+            return True
+    return False
+
+#print(listaDePalabras(["a","aa","aaa","aaaaaaaaaaa"]))
+## Ejercicio 6
+def esPalindroma(palabra:str)->bool:
+    for i in range(len(palabra)//2):
+        if palabra[i] != palabra[len(palabra)-i-1]:
+            return False
+    return True
+
+print(esPalindroma("aibofobia"))
+## Itero sobre la primera mitad del string, en cada iteracion se compara el caracter en la posicion actual con el caracter en la posicion correspondiente de la otra punta del string, si no son iguales da false
+
+## Ejercicio 7
+#def analizarPass(clave:str)->str:
+    #for caracter in clave:
+        #if caracter.islower():
+            #usaMinuscula
+# Preguntar si se puede utilizar islower is upper isdigit etc.
