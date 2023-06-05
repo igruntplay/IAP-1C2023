f3f244936f8964093ca66c058b6b20b5bee5af82
Gaspar <gaspar.onesto@gmail.com>
2023-06-05 00:12:37 -0300

    agrego ejercicio 16
    me falta el ultimo punto

---
+++ Practica-4/ejercicio16.hs
@@ -0,0 +1,24 @@
+menorDivisor :: Integer -> Integer
+menorDivisor n = menorDivisorAux n 2
+
+menorDivisorAux :: Integer -> Integer -> Integer
+menorDivisorAux n m | n `mod` m == 0 = m
+                    | otherwise = menorDivisorAux n (m+1)
+
+{-Recordar que un primeo es aquel numero mayor a 1 que solo se pueda dividir por sí mismo-}
+esPrimo :: Integer -> Bool
+esPrimo n | menorDivisor n == n = True
+          | otherwise = False
+
+maximoComunDivisor :: Integer -> Integer -> Integer
+maximoComunDivisor n m | n == m = n
+                       | n == 1 || m == 1 = 1
+                       | otherwise = maximoComunDivisor m (m `mod` n)
+
+sonCoprimos :: Integer -> Integer -> Bool
+sonCoprimos n m | (maximoComunDivisor n m) == 1 = True
+                | otherwise = False
+
+{- incompleto
+nEsimoPrimo :: Integer -> Integer
+nEsimoPrimo n = nEsimoPrimoAux-}
