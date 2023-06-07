fe8dea1484c3ac6870bbdd8dddebc77b4830459d
Gaspar <gaspar.onesto@gmail.com>
2023-06-06 23:17:58 -0300

    me ayudó moncho

--- Practica-5/Ejercicio-6/ejercicio3.hs
+++ Practica-5/Ejercicio-6/ejercicio3.hs
@@ -1,8 +1,10 @@
-{-
-Preguntar
--}
 type Set a = [a]
 
 productoCartesiano :: Set Integer -> Set Integer -> Set (Integer, Integer)
-productoCartesiano [] [] = []
-productoCartesiano (x:xs) (y:ys) | 
+productoCartesiano [] _ = []
+productoCartesiano _ [] = []
+productoCartesiano (x:xs) ys = productoCartesianoUnoConTodos x ys ++ productoCartesiano xs ys
+
+productoCartesianoUnoConTodos :: Integer -> Set Integer -> Set (Integer, Integer)
+productoCartesianoUnoConTodos _ [] = []
+productoCartesianoUnoConTodos x (y:ys) = (x, y) : productoCartesianoUnoConTodos x ys
