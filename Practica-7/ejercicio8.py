eb0c0104e26db275c3d0d4262a041f5ef9210111
Gaspar <gaspar.onesto@gmail.com>
2023-06-07 18:40:52 -0300

    subo ejercicio 8 de la guia 7

---
+++ Practica-7/ejercicio8.py
@@ -0,0 +1,26 @@
+# Primer bloque de código
+x = 5  # Aquí estamos declarando la variable x e inicializándola con 5
+y = 7  # Aquí estamos declarando la variable y e inicializándola con 7
+# En este punto, x es 5 e y es 7
+
+# Segundo bloque de código
+x = 5  # Nuevamente inicializamos x con 5
+y = 7  # Nuevamente inicializamos y con 7
+z = x + y  # Declaramos z y la inicializamos con la suma de x e y
+# En este punto, x es 5, y es 7 y z es 12 (5 + 7)
+
+# Tercer bloque de código
+x = 5  # Inicializamos x con 5
+x = "hora"  # Reasignamos x con el valor "hora"
+# En este punto, x es "hora". La asignación anterior de 5 a x se ha perdido
+
+# Cuarto bloque de código
+x = True  # Inicializamos x con el valor booleano True
+y = False  # Inicializamos y con el valor booleano False
+res = x and y  # Declaramos res e inicializamos con el resultado de la operación 'and' entre x e y
+# En este punto, x es True, y es False, y res es False (porque True AND False es False)
+
+# Quinto bloque de código
+x = False  # Inicializamos x con el valor booleano False
+res = not(x)  # Declaramos res e inicializamos con el resultado de aplicar 'not' a x
+# En este punto, x es False y res es True (porque NOT False es True)
