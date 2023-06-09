fc201edb242324434f6b884ce183af85c4d8bfc9
igruntplay <gaspar.onesto@gmail.com>
2023-06-09 14:54:31 -0300

    agrego ejercicio

--- Practica-5/Ejercicio-5/ejercicio1.hs
+++ Practica-5/Ejercicio-5/ejercicio1.hs
@@ -5,6 +5,10 @@ nat2binAux :: Integer -> [Integer]
 nat2binAux n | n < 2 = [n] 
       | otherwise = [mod n 2] ++ nat2binAux (div n 2)
 
+{-
+      Con el auxiliar, basta  y sobra.
+      Reverso y nat2bin, son innecesarios.
+-}
 reverso :: [t] -> [t]
 reverso [] = []
 reverso (x:xs) = reverso xs ++ [x]
